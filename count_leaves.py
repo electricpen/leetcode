a36bac054a337b8f6e9406f386f3c2c5f45ea695
Jonathan Ling <electric.pen@gmail.com>
2017-11-21 10:46:35 -0800

    Bug fix count leaves python

--- count_leaves.py
+++ count_leaves.py
@@ -10,7 +10,9 @@ class BinaryTree:
         self.root = root_node
 
     def number_of_leaves(self, root):
-        if root.left_child is None and root.right_child is None:
+        if root == None:
+            return 0
+        elif root.left_child is None and root.right_child is None:
             return 1
         else:
             return self.number_of_leaves(root.left_child) + self.number_of_leaves(root.right_child)
