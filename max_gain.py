95487a2675b38c11c58d5e2d8a925248033927b5
Jonathan Ling <electric.pen@gmail.com>
2017-11-22 10:59:43 -0800

    implement max gain python

---
+++ max_gain.py
@@ -0,0 +1,17 @@
+def max_gain(input_list):
+    """
+    iterate through the list, for each item
+      iterate through the remaining items with index above the current item
+        calculate the difference in values and compare to a tracking value
+          if it is larger, replace tracking value with current value
+    """
+    largest = 0
+    for index, first in enumerate(input_list):
+        for second in input_list[index + 1:]:
+            diff = second - first
+            if diff > largest:
+                largest = diff
+    return largest
+
+
+print(max_gain([1, 2, 3, 4, 5, 6, 10]))
