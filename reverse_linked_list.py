b1428cd4d705a16e4d0517f1036c36866d05ffb1
Jonathan Ling <electric.pen@gmail.com>
2017-11-24 14:44:13 -0800

    Added support class and methods for debugging

--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -1,3 +1,18 @@
+class Node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+
+    def setData(self, data):
+        self.data = data
+
+    def getData(self):
+        return self.data
+
+    def setNext(self, next):
+        self.next = next
+
+
 class SinglyLinkedList:
     # constructor
     def __init__(self):
@@ -5,16 +20,25 @@ class SinglyLinkedList:
 
     # method for setting the head of the Linked List
     def setHead(self, head):
-        self.head = head
+        self.head = Node()
+        self.head.setData(head)
+
+    def show(self):
+        pointer = self.head
+        while pointer != None:
+            print(pointer.getData())
+            pointer = pointer.next
 
     def insert(self, data):
         pointer = self.head
-        if self.head == None:
+        new_node = Node()
+        new_node.setData(data)
+        if self.head is None:
             self.setHead(data)
         else:
             while pointer.next != None:
                 pointer = pointer.next
-            pointer.setData(data)
+            pointer.next = new_node
 
     def reverse(self):
         stack = []
@@ -25,11 +49,19 @@ class SinglyLinkedList:
             pointer = pointer.next
         while len(stack) > 0:
             reverse_list.insert(stack.pop())
+        reverse_list.show()
+        self = reverse_list
 
 
-’’’
-built in node methods
-getData
-setData
-setNext
-’’’
+test = SinglyLinkedList()
+print('Should be empty:')
+test.show()
+test.insert(1)
+print('Should be 1')
+test.show()
+test.insert(2)
+print('Should be 1, 2.')
+test.show()
+test.reverse()
+print('Should be 2, 1')
+test.show()
