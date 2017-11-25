0079a9739277c1204e5b7157c0892b29b21483d7
Jonathan Ling <electric.pen@gmail.com>
2017-11-25 01:23:21 -0800

    Bug fix merge ranges python

--- merge_ranges.py
+++ merge_ranges.py
@@ -3,10 +3,13 @@ def merge_ranges(input_range_list):
     item = input_range_list[0]
     next_item = input_range_list[1]
     while index < len(input_range_list) - 1:
-        if item.upper_bound > next_item.lower_bound:
+        if item.upper_bound >= next_item.lower_bound:
             if item.upper_bound < next_item.upper_bound:
                 item.upper_bound = next_item.upper_bound
             input_range_list.pop(index + 1)
+            next_item = input_range_list[index + 1]
         else:
             index += 1
+            item = next_item
+            next_item = input_range_list[index + 1]
     return input_range_list
