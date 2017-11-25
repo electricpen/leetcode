315752849fedcdc2de2428a41eeada6570fce548
Jonathan Ling <electric.pen@gmail.com>
2017-11-24 18:00:20 -0800

    Implement merge ranges python

---
+++ merge_ranges.py
@@ -0,0 +1,12 @@
+def merge_ranges(input_range_list):
+    index = 0
+    item = input_range_list[0]
+    next_item = input_range_list[1]
+    while index < len(input_range_list) - 1:
+        if item.upper_bound > next_item.lower_bound:
+            if item.upper_bound < next_item.upper_bound:
+                item.upper_bound = next_item.upper_bound
+            input_range_list.pop(index + 1)
+        else:
+            index += 1
+    return input_range_list
