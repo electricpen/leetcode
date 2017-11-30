a90e90480c2e01ecc6896d51e86872f27cd4af0b
Jonathan Ling <electric.pen@gmail.com>
2017-11-29 17:31:57 -0800

    Implement breadth first graph javascript

--- breadthFirstGraph.js
+++ breadthFirstGraph.js
@@ -18,4 +18,32 @@ class Node {
   }
 }
 
-const breadthFirstSearch = vertex => {};
+class Queue {
+  constructor() {
+    self.storage = [];
+  }
+
+  enqueue(data) {
+    self.storage.push(data);
+  }
+
+  dequeue() {
+    return self.storage.shift();
+  }
+
+  size() {
+    return self.storage.length;
+  }
+}
+
+const breadthFirstSearch = (vertex, callback) => {
+  let queue = new Queue();
+  queue.enqueue(vertex);
+  while (queue.size > 0) {
+    let current = queue.dequeue();
+    callback(current);
+    for (edge of current) {
+      queue.enqueue(edge);
+    }
+  }
+};
