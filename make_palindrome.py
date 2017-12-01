42e68d8057e3996b3369fcfc867938be6268bc56
Jonathan Ling <electric.pen@gmail.com>
2017-12-01 12:08:57 -0800

    Add recursive solution note to make palindrome

--- make_palindrome.py
+++ make_palindrome.py
@@ -2,14 +2,19 @@ def make_palindrome(input):
     def is_palindrome(substr):
         return substr == substr[::-1]
     for index in range(len(input)):
-        """
-        make a slice of input = last len-index chars
-        check to see if it is a palindrome
-        if it is, return input + first index chars reversed concat onto input
-        """
         if is_palindrome(input[index:]):
             append = input[0:index][::-1]
             return input + append
 
 
-print(make_palindrome('ab'))
+print(make_palindrome('race'))
+
+
+"""
+interesting recursive solution:
+def make_palindrome(input):
+    if input == input[::-1]:
+        return input
+    else:
+        return input[0] + make_palindrome(input[1:]) + input[0]
+"""
