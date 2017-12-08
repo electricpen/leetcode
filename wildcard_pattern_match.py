328e1f5d55078a4dda882ab6daf790ae877d8b47
Jonathan Ling <electric.pen@gmail.com>
2017-12-08 13:29:18 -0800

    Code refactor wildcard pattern match python

--- wildcard_pattern_match.py
+++ wildcard_pattern_match.py
@@ -7,23 +7,9 @@ def match(first, second):
                 pointer2 += 1
             if pointer2 == len(second):
                 return False
-            else:
-                pointer1 += 1
-                pointer2 += 1
-        elif first[pointer1] == '?':
-            pointer1 += 1
-            pointer2 += 1
-        elif first[pointer1] != second[pointer2]:
+        elif first[pointer1] != second[pointer2] and first[pointer1] != '?':
             return False
-    return pointer1 == len(first) - 1
+        pointer1 += 1
+        pointer2 += 1
 
-
-"""
-iterate over first
-  if char is not a wildcard
-    match with pointer at second
-  else
-    if it is a ?, increment both pointers
-    if it is a *, increment second pointer until it finds the char after the *
-  
-"""
+    return pointer1 == len(first) and pointer2 == len(second)
