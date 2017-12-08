8bd53fd4e596f1dde52954585125eb2bc6ce74c4
Jonathan Ling <electric.pen@gmail.com>
2017-12-08 14:48:27 -0800

    Refactor for O(1) space and O(n) time

--- rotate-array.py
+++ rotate-array.py
@@ -1,6 +1,9 @@
 def rotate_left(list_numbers, k):
     offset = k % len(list_numbers)
-    return list_numbers[offset:] + list_numbers[:offset]
+    for i in range(offset):
+        temp = list_numbers.pop(0)
+        list_numbers.append(temp)
+    return list_numbers
 
 
 print(rotate_left([1, 2, 3, 4, 5, 6, 7, 8, 9], 2))
