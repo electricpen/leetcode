1b3b549894ea54de56262fb4b73f74e58fd76146
Jonathan Ling <electric.pen@gmail.com>
2017-12-17 21:37:16 -0800

    Bug fix happy number python

--- happy_number.py
+++ happy_number.py
@@ -1,10 +1,16 @@
+from functools import reduce
+
+
 def is_happy_number(number, sequence={}):
-    if sequence[number]:
+    if number in sequence:
         return False
-    digits = str(number).split('')
-    next_num = reduce(lambda x, y: x * x + y * y, number)
+    digits = [int(d) for d in str(number)]
+    next_num = reduce(lambda x, y: x * x + y * y, digits)
     sequence[number] = next_num
     if number == 1:
         return True
     else:
         return is_happy_number(next_num, sequence)
+
+
+print(is_happy_number(100))
