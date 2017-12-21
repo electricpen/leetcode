f0c1fa8b134e052b2c32291b3d3cb395546d5cf5
Jonathan Ling <electric.pen@gmail.com>
2017-12-20 19:49:57 -0800

    Debug smallestDistancePairs.js

--- smallestDistancePairs.js
+++ smallestDistancePairs.js
@@ -1,13 +1,26 @@
 var smallestDistancePair = function(nums, k) {
+  var sum = (acc, val) => {
+    return Math.abs(acc - val);
+  };
+  let results = {};
+  let diff = [];
+  // k =
   const numericSort = (a, b) => {
     return a - b;
   };
-  nums.sort();
-  if (k < nums.length - 2) {
-    return Math.abs(nums[0] - nums[k]);
-  } else {
-    return Math.abs(nums[0] - nums[nums.length - 1]);
+  nums.sort(numericSort);
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i !== j) {
+        results[nums[i] + " " + nums[j]] = true;
+      }
+    }
   }
+  for (pair in results) {
+    diff.push(pair.split(" ").reduce(sum));
+  }
+  diff.sort(numericSort);
+  return diff[k - 1] === undefined ? diff[diff.length - 1] : diff[k - 1];
 };
 
 console.log(smallestDistancePair([1, 6, 1], 3));
