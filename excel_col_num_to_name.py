374ce61cf65c37ff262f302d74d048ae53f07efd
Jonathan Ling <electric.pen@gmail.com>
2017-12-21 22:30:11 -0800

    Bug fix num to name python

--- excel_col_num_to_name.py
+++ excel_col_num_to_name.py
@@ -1,6 +1,7 @@
 def excel_column_number_to_name(column_number):
     if column_number < 27:
-        return str(column_number + 64)
+        return chr(column_number + 64)
     else:
-        letter = str(column_number % 26) if column_number % 26 != 0 else str(26)
-        return str(excel_column_number_to_name(column_number // 26)) + letter
+        letter = chr((column_number %
+                      26) + 64) if column_number % 26 != 0 else chr(90)
+        return excel_column_number_to_name((column_number - 1) // 26) + letter
