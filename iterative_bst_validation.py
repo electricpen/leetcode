74569a2ee15a56f7fe2799d23cf21f3ddba68772
Jonathan Ling <electric.pen@gmail.com>
2017-12-24 09:26:45 -0800

    Implement stack class and methods for interative bst

--- iterative_bst_validation.py
+++ iterative_bst_validation.py
@@ -1,4 +1,18 @@
 # Collections module has already been imported.
+class Stack:
+    def __init__(self):
+        self.storage = []
+        self.size = 0
+
+    def push(self, data):
+        self.size += 1
+        self.storage.push(data)
+
+    def pop(self):
+        self.size -= 1
+        return self.storage.pop(0)
+
+
 class BinaryTree:
     def __init__(self, root_node=None):
         self.root = root_node
@@ -6,5 +20,8 @@ class BinaryTree:
     def validate_BST_Itr(self, root):
         # Return type should be Boolean
         is_bst = True
+        stack = Stack()
+        stack.push(root)
+        while is_bst is True and stack.size > 0:
 
         return is_bst
