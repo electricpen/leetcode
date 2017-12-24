7f5c6d1ae528af20b60e5bbdd0ba8ae2f922d7b5
Jonathan Ling <electric.pen@gmail.com>
2017-12-24 09:14:36 -0800

    Implement iterative bst validation python

---
+++ iterative_bst_validation.py
@@ -0,0 +1,10 @@
+# Collections module has already been imported.
+class BinaryTree:
+    def __init__(self, root_node=None):
+        self.root = root_node
+
+    def validate_BST_Itr(self, root):
+        # Return type should be Boolean
+        is_bst = True
+
+        return is_bst
