507e04fb2a1b4079e3c5549230277127f40ed84e
Jonathan Ling <electric.pen@gmail.com>
2017-12-31 00:16:01 -0800

    Bug fix find middle linked list python

--- find_middle_linked_list.py
+++ find_middle_linked_list.py
@@ -22,9 +22,36 @@ class SinglyLinkedList:
     def setHead(self, head):
         self.head = head
 
-    def find_middle_node(self): ß
-        mid = self.size()
+    def size(self):
+        count = 0
+        curr = self.head
+        while curr is not None:
+            count += 1
+            curr = curr.next
+        return count
+
+    def initialize(self, data):
+        for item in data:
+            new_node = Node()
+            new_node.setData(item)
+            if self.head == None:
+                self.head = new_node
+                curr = self.head
+            else:
+                curr.next = new_node
+                curr = curr.next
+
+    def find_middle_node(self):
+        if self.head is None:
+            return None
+        mid = self.size() // 2
         curr = self.head
         for index in range(mid):
             curr = curr.next
         return curr.getData()
+
+
+test = SinglyLinkedList()
+test.initialize([1, 2, 3, 4])
+print(test.size())
+print(test.find_middle_node())
