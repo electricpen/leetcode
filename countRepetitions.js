09345d925f47dc70f0ca211d0e4906517cbf3f9d
Jonathan Ling <electric.pen@gmail.com>
2018-01-26 19:38:36 -0800

    Stub countRepititions and spiralTraversal

---
+++ countRepetitions.js
@@ -0,0 +1,6 @@
+var getMaxRepetitions = function(s1, n1, s2, n2) {
+  // find the number of times S2 occurs in S1
+  // check if the characters in s1 occur in s2
+  // if not return 0
+  // if yes check sequence
+};
