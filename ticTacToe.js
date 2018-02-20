c314a8040398c759f906fc5bd1827c1d9c5b7ed0
Jonathan Ling <electric.pen@gmail.com>
2018-02-20 14:50:19 -0800

    Implement draw function for board

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,16 +1,23 @@
 class TicTacToe {
   constructor(props) {
+    this.x = " X ";
+    this.o = " O ";
+    this.n = "   ";
     this.players = ["player1", "player2"];
-    this.board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]];
+    this.board = [
+      [this.n, this.n, this.n],
+      [this.n, this.n, this.n],
+      [this.n, this.n, this.n]
+    ];
     this.turn = this.players[0];
   }
 
   draw() {
     let stringifiedBoard = [];
-    for (row in this.board) {
+    this.board.forEach(row => {
       stringifiedBoard.push(row.join("|"));
-    }
-    stringifiedBoard.join("\n-----");
+    });
+    stringifiedBoard = stringifiedBoard.join("\n-----------\n");
     console.log(stringifiedBoard);
   }
 
---
+++ waterBlocks.js
@@ -0,0 +1,9 @@
+const findValley = (towers, place) => {
+  // expand out searching for higher points on either side
+  // return true if we are the lowest point and false if notZ
+};
+
+const waterBlocks = towers => {
+  // find lowest number that has edges
+  // return 1 + list with that number incremented by 1
+};
